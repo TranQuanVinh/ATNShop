68e90e7cb368fe41866ce72b1df34a49d1baec00
TranQuanVinh <91666073+TranQuanVinh@users.noreply.github.com>
2021-11-19 12:16:09 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,7 +1,8 @@
 <?php
-define("DB_HOST", "remotemysql.com");
-define("DB_USER", "Ze6g90qJCC");
-define("DB_PASS", "DovBD498TN");
-define("DB_NAME", "Ze6g90qJCC");
+define("DB_HOST", "ec2-3-208-157-78.compute-1.amazonaws.com
+");
+define("DB_USER", "giujnnyyzwrfwa");
+define("DB_PASS", "fd1ca79a39f460594b2a23a0bc3dea741e92918adc2e837bc55ef71c31a4eec3");
+define("DB_NAME", "d82s4vgfr634c2");
 
 ?>
