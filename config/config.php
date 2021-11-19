3f14ab748839c18d3dc088c8783c35cc6f7f42df
TranQuanVinh <91666073+TranQuanVinh@users.noreply.github.com>
2021-11-19 13:35:10 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,5 +1,9 @@
 <?php
-$Connect = pg_connect("postgres://giujnnyyzwrfwa:fd1ca79a39f460594b2a23a0bc3dea741e92918adc2e837bc55ef71c31a4eec3@ec2-3-208-157-78.compute-1.amazonaws.com:5432/d82s4vgfr634c2
+define("DB_HOST", "ec2-3-208-157-78.compute-1.amazonaws.com
 ");
+define("DB_USER", "giujnnyyzwrfwa");
+define("DB_PASS", "fd1ca79a39f460594b2a23a0bc3dea741e92918adc2e837bc55ef71c31a4eec3
+");
+define("DB_NAME", "d82s4vgfr634c2");
+
 ?>
-    
